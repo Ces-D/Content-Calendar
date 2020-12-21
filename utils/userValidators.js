2488c3586610584b36de8aca42ac80dceb9de3ed
Cesar <cesdia883@gmail.com>
2020-12-20 19:14:24 -0500

    discovered optional field for express-validator

--- utils/userValidators.js
+++ utils/userValidators.js
@@ -33,37 +33,23 @@ const registerValidators = [
 ];
 
 const updateValidators = [
-    body("email").custom((email) => {
-        if (email) {
-            email
-                .isEmail()
-                .withMessage("Must Be A Valid Email")
-                .normalizeEmail();
-        }
-        return true;
-    }),
-    body("password").custom((password) => {
-        if (password) {
-            password
-                .isLength({ min: 8 })
-                .withMessage("Must Be 8 Characters Or More")
-                .matches("[0-9]")
-                .withMessage("Must Contain Numbers")
-                .matches("[A-Z]")
-                .withMessage("Must Contain Capital Letters");
-        }
-        return true;
-    }),
-    body("displayName").custom((displayName) => {
-        if (displayName) {
-            displayName
-                .notEmpty()
-                .withMessage("Cannot Be Empty")
-                .isLength({ min: 2 })
-                .withMessage("Must Be At Least 2 Character Long");
-        }
-        return true;
-    }),
+    body("email")
+        .optional()
+        .isEmail()
+        .withMessage("Must Be A Valid Email")
+        .normalizeEmail(),
+    body("password")
+        .optional()
+        .isLength({ min: 8 })
+        .withMessage("Must Be 8 Characters Or More")
+        .matches("[0-9]")
+        .withMessage("Must Contain Numbers")
+        .matches("[A-Z]")
+        .withMessage("Must Contain Capital Letters"),
+    body("displayName")
+        .optional()
+        .isLength({ min: 2 })
+        .withMessage("Must Be At Least 2 Character Long"),
 ];
 
 module.exports = { loginValidators, registerValidators, updateValidators };
